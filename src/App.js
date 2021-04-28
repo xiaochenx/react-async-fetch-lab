4d2ca81e7b4a21fe77ffd7e86711a25123973106
Xiaochen Xu <xiaochex@gmail.com>
2021-04-28 13:36:22 -0400

    commit tree da5852355f1a35b94a3a49c4384a79a4ce27f566

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+    componentDidMount(){
+        fetch("http://api.open-notify.org/astros.json")
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data)
+        })
+    }
+}
